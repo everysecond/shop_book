2bb4797687415e7c51516738e41de4c56e19e6c7
lidezhu <540889445@qq.com>
2021-01-08 13:06:41 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(766);
+        Schema::defaultStringLength(191);
         $this->validatorExtend();
     }
 
