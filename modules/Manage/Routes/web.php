fda0d4acba6a3dbbe145ade318d6981261ecbdd5
lidezhu <540889445@qq.com>
2021-01-08 15:35:27 +0800

    fix

--- modules/Manage/Routes/web.php
+++ modules/Manage/Routes/web.php
@@ -24,6 +24,7 @@ Route::group([
     'middleware' => [],
     'prefix'     => 'manage'
 ], function () {
+    dd(222222);
     //后台首页
     Route::get('/', 'ManageController@index')->name('manage.index');
     Route::get('console', 'ManageController@console')->name('manage.console');
